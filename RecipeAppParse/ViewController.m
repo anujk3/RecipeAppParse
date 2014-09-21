43059db0fcd6da9f445b73d56f8db92358814f84
Anuj Katiyal <anujk3@gmail.com>
2014-09-21 21:09:45 +0530

    Initial Commit

---
+++ RecipeAppParse/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RecipeAppParse
+//
+//  Created by Anuj Katiyal on 21/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
